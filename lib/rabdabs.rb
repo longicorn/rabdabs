822665a5cafeb6b9cc17d3854d551baf01e3e2a3
longicorn <longicorn.c@gmail.com>
2024-09-14 22:58:40 +0900

    init

---
+++ lib/rabdabs.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "rabdabs/version"
+
+module Rabdabs
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/rabdabs/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module Rabdabs
+  VERSION = "0.1.0"
+end
